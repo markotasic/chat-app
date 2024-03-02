6dadb7c18edc8f7bfdb6fc2f4e5d5a28fc0120a2
Marko Tasic <m4rko2001@gmail.com>
2024-03-02 20:09:39 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Chat App',
+  description: 'A simple application created by Marko Tasic 😎'
+}
+
+const RootLayout = ({
+  children
+}: Readonly<{ children: React.ReactNode }>): JSX.Element => {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
+
+export default RootLayout
---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+const Home = (): JSX.Element => {
+  return <div>hello world</div>
+}
+
+export default Home
